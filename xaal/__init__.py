295e7eab94c0caa8d8900ae4984dcf066aad46c0
jkerdreu <jkerdreu@b32b6428-25c9-4566-ad07-03861ab6144f>
2022-09-15 20:41:52 +0000

    Fix start / stop in AsyncEngine related
    git-svn-id: https://redmine.imt-atlantique.fr/svn/xaal/code/Python/branches/0.7@2979 b32b6428-25c9-4566-ad07-03861ab6144f

--- xaal/__init__.py
+++ xaal/__init__.py
@@ -1,20 +1,24 @@
 
-
-import asyncio
+import logging
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from . import bridge
+from .bridge import Bridge
 from .const import DOMAIN
+
+
+_LOGGER = logging.getLogger(__name__)
 
 PLATFORMS: list[str] = ["light", "switch", "sensor", "binary_sensor"]
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # create the hub and load the platforms
-    br = bridge.Bridge(hass)
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = br
-    await br.wait_is_ready()
-    print("READY")
+    bridge = Bridge(hass)
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = bridge
+
+    await bridge.wait_is_ready()
+    _LOGGER.debug("xAAL Bridge READY")
     hass.config_entries.async_setup_platforms(entry, PLATFORMS)
     return True
 
@@ -23,7 +27,10 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # stop xAAL tasks before removing reference
     # FIXME: some tasks never ends: RecvQ, Timers
     await hass.data[DOMAIN][entry.entry_id].engine.stop()
+    _LOGGER.debug("Unloading xAAL platforms")
     unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
+    else:
+        _LOGGER.error("Unable to unload xAAL platforms")
     return unload_ok
