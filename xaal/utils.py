b2a85e496e8b4476e9855bb2d774c115bdc07ce0
jkerdreu <jkerdreu@b32b6428-25c9-4566-ad07-03861ab6144f>
2022-11-05 23:27:56 +0000

    - Fix a wrong type hinting.
    git-svn-id: https://redmine.imt-atlantique.fr/svn/xaal/code/Python/branches/0.7@3039 b32b6428-25c9-4566-ad07-03861ab6144f

--- xaal/utils.py
+++ xaal/utils.py
@@ -1,10 +1,8 @@
-from typing import Tuple
-
 def str_to_id(value: str) -> str:
     return value.translate ({ord(c): "_" for c in "!@#$%^&*()[]{};:,./<>?\|`~-=_+ "})
 
 
-def get_dev_identifiers(identifiers: str) -> Tuple(str, str):
+def get_dev_identifiers(identifiers: str) -> tuple[str, str]:
     ident_data = list(identifiers)
     domain = ident_data[0][0]
     dev_ident = ident_data[0][1]
