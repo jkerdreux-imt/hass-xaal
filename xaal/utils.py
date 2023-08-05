6eb2c11c6ac758dc8c4614c70b7f89c93109896c
jkerdreu <jkerdreu@b32b6428-25c9-4566-ad07-03861ab6144f>
2023-08-05 00:08:59 +0000

    Fix an error w/ a empty identifier list
    git-svn-id: https://redmine.imt-atlantique.fr/svn/xaal/code/Python/branches/0.7@3069 b32b6428-25c9-4566-ad07-03861ab6144f

--- xaal/utils.py
+++ xaal/utils.py
@@ -1,7 +1,10 @@
 
 def get_dev_identifiers(identifiers: str) -> tuple[str, str]:
+    # import pdb;pdb.set_trace()
     ident_data = list(identifiers)
-    domain = ident_data[0][0]
-    dev_ident = ident_data[0][1]
-    return (domain, dev_ident)
-
+    try:
+        domain = ident_data[0][0]
+        dev_ident = ident_data[0][1]
+        return (domain, dev_ident)
+    except IndexError:
+        return ("", "")
